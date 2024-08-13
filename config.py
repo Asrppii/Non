2619e3dfb2ba0e783de987502c4663f1c5107c1f
Asrppii <asarppkunjutty@gmail.com>
2024-08-13 16:29:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,19 +6,9 @@ class Config(object):
     # The Telegram API things
     API_ID = int(os.environ.get("API_ID", 12345))
     API_HASH = os.environ.get("API_HASH")
-    # Get these values from my.telegram.org
-    # the download location, where the HTTP Server runs
-    DOWNLOAD_LOCATION = "./DOWNLOADS"
-    # Telegram maximum file upload size
-    MAX_FILE_SIZE = 50000000
-    TG_MAX_FILE_SIZE = 4194304000 #2097152000
-    FREE_USER_MAX_FILE_SIZE = 50000000
+    # Get these values from my.telegram.org    
     # chunk size that should be used with requests
     CHUNK_SIZE = int(128)
-    # default thumbnail to be used in the videos
-    # proxy for accessing youtube-dl in GeoRestricted Areas
-    # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
-    HTTP_PROXY = ""
     # maximum message length in Telegram
     MAX_MESSAGE_LENGTH = 4096
     # set timeout for subprocess
@@ -26,7 +16,4 @@ class Config(object):
     # your telegram id
     OWNER_ID = int(os.environ.get("OWNER_ID", ""))
     SESSION_NAME = "UPLOADER-X-BOT"
-    # database uri (mongodb)
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
-    MAX_RESULTS = "50"
-    PREMIUM_USER = os.environ.get("PREMIUM_USER")
+  
